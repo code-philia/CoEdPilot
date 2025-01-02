ccf4ae997a4dea8b3375f9ae012f757b12d64939
Lint Action <lint-action@samuelmeuli.com>
2025-01-02 17:01:49 +0000

    Fix code style issues with Black

--- file_locator/dependency_analyzer.py
+++ file_locator/dependency_analyzer.py
@@ -16,15 +16,18 @@ from typing import Union
 
 
 class DependencyAnalyzer(nn.Module, PyTorchModelHubMixin):
-    def __init__(self, encoder: Union[PreTrainedModel, None] = None,
-                 match_tokenizer: Union[RobertaTokenizerFast, None] = None,
-                 input_features: int = 768,
-                 output_features: int = 2):
+    def __init__(
+        self,
+        encoder: Union[PreTrainedModel, None] = None,
+        match_tokenizer: Union[RobertaTokenizerFast, None] = None,
+        input_features: int = 768,
+        output_features: int = 2,
+    ):
         super(DependencyAnalyzer, self).__init__()
         if not encoder:
             encoder: PreTrainedModel = (
                 EncoderDecoderModel.from_encoder_decoder_pretrained(
-                    'microsoft/codebert-base', 'microsoft/codebert-base'
+                    "microsoft/codebert-base", "microsoft/codebert-base"
                 ).encoder
             )
         if match_tokenizer:
@@ -37,45 +40,45 @@ class DependencyAnalyzer(nn.Module, PyTorchModelHubMixin):
         self.dense = nn.Linear(input_features, output_features)
 
     def forward(self, input_ids, attention_mask):
-        outputs = self.encoder(input_ids=input_ids,
-                               attention_mask=attention_mask)
+        outputs = self.encoder(input_ids=input_ids, attention_mask=attention_mask)
         pooler_output = outputs.pooler_output
         output_2d = self.dense(pooler_output)
         return output_2d
 
 
 def load_model_and_tokenizer(
-    model_dir: str, directly_load: bool = True, model_with_structure_dir: str | None = None,
+    model_dir: str,
+    directly_load: bool = True,
+    model_with_structure_dir: str | None = None,
 ) -> tuple[DependencyAnalyzer, RobertaTokenizerFast]:
     if directly_load:
         # 增强模型和分词器加载时的异常处理
         try:
             tokenizer = RobertaTokenizerFast.from_pretrained(model_dir)
         except Exception as e:
-            raise RuntimeError(
-                f"Failed to load tokenizer from {model_dir}:{e}")
+            raise RuntimeError(f"Failed to load tokenizer from {model_dir}:{e}")
         if model_with_structure_dir:
-            model = DependencyAnalyzer.from_pretrained(
-                model_with_structure_dir)
+            model = DependencyAnalyzer.from_pretrained(model_with_structure_dir)
         else:
             model = DependencyAnalyzer(match_tokenizer=tokenizer)
-            model.load_state_dict(torch.load(
-                os.path.join(model_dir, 'pytorch_model.bin')))
+            model.load_state_dict(
+                torch.load(os.path.join(model_dir, "pytorch_model.bin"))
+            )
         return model, tokenizer
 
     model = EncoderDecoderModel.from_pretrained(model_dir)
     if not isinstance(model, EncoderDecoderModel):
-        raise RuntimeError(f'Model read from {model_dir} is not valid')
+        raise RuntimeError(f"Model read from {model_dir} is not valid")
     model = model.encoder
     if not isinstance(model, PreTrainedModel):
-        raise RuntimeError(f'Encoder of original model is not valid')
+        raise RuntimeError(f"Encoder of original model is not valid")
 
     tokenizer: RobertaTokenizerFast = RobertaTokenizerFast.from_pretrained(
-        'microsoft/codebert-base'
+        "microsoft/codebert-base"
     )
     if not isinstance(tokenizer, RobertaTokenizerFast):
-        raise RuntimeError('Cannot read tokenizer as microsoft/codebert-base')
-    special_tokens = ['<from>', '<to>']
+        raise RuntimeError("Cannot read tokenizer as microsoft/codebert-base")
+    special_tokens = ["<from>", "<to>"]
     # tokenizer.add_tokens(my_tokens, special_tokens = False)
     tokenizer.add_tokens(special_tokens, special_tokens=True)
 
@@ -87,13 +90,12 @@ def load_model_and_tokenizer(
 class DependencyClassifier:
     def __init__(
         self,
-        load_dir: str = '../dependency_analyzer/model',
+        load_dir: str = "../dependency_analyzer/model",
         load_with_model_struture: bool = False,
-        device: torch.device = torch.device('cuda', index=0),
+        device: torch.device = torch.device("cuda", index=0),
     ):
         self.model, self.tokenizer = (
-            load_model_and_tokenizer(
-                load_dir, model_with_structure_dir=load_dir)
+            load_model_and_tokenizer(load_dir, model_with_structure_dir=load_dir)
             if load_with_model_struture
             else load_model_and_tokenizer(load_dir)
         )
@@ -105,7 +107,7 @@ class DependencyClassifier:
             raise ValueError("Both code_1 and code_2 must be strings.")
         if not code_1 or not code_2:
             raise ValueError("Both code_1 and code_2 must not be empty.")
-        return '<from>' + code_1 + '<to>' + code_2
+        return "<from>" + code_1 + "<to>" + code_2
 
     def construct_corpus_pair(self, corpus: list[tuple[str, str]]):
         return [self.construct_pair(code_1, code_2) for code_1, code_2 in corpus]
@@ -115,14 +117,14 @@ class DependencyClassifier:
             raise ValueError("Input text must be a string.")
         text = text.strip()  # 去除首尾空白字符
         # ATTENTION: converted to batch here
-        token_input = self.tokenizer(text, return_tensors='pt')
+        token_input = self.tokenizer(text, return_tensors="pt")
         if torch.cuda.is_available():
             token_input = token_input.to(self.device)
 
         with torch.no_grad():
             outputs = self.model(
-                input_ids=token_input['input_ids'],
-                attention_mask=token_input['attention_mask'],
+                input_ids=token_input["input_ids"],
+                attention_mask=token_input["attention_mask"],
             )[0]
         outputs = torch.sigmoid(outputs).detach().cpu()
         return outputs[1]
@@ -130,45 +132,49 @@ class DependencyClassifier:
     def batch_gen(self, corpus_pair: list[str]):
         sigmoid = nn.Sigmoid()
         token_input = self.tokenizer(
-            corpus_pair, return_tensors='pt', padding=True, truncation=True, max_length=512,
+            corpus_pair,
+            return_tensors="pt",
+            padding=True,
+            truncation=True,
+            max_length=512,
         )
-        dataset = TensorDataset(
-            token_input['input_ids'], token_input['attention_mask'])
+        dataset = TensorDataset(token_input["input_ids"], token_input["attention_mask"])
         dataloader = DataLoader(dataset, batch_size=32, shuffle=False)
 
         preds = []
         with torch.no_grad():
             for batch in dataloader:
-                batch_input, attention_mask = [
-                    item.to(self.device) for item in batch]
-                outputs = self.model(input_ids=batch_input,
-                                     attention_mask=attention_mask)
+                batch_input, attention_mask = [item.to(self.device) for item in batch]
+                outputs = self.model(
+                    input_ids=batch_input, attention_mask=attention_mask
+                )
                 outputs = sigmoid(outputs)[:, 1]
                 preds.append(outputs.detach().cpu())
         preds = torch.cat(preds, dim=0)
         return preds.numpy()
 
 
-def cal_dep_score(hunk: dict, file_content: str, dependency_analyzer: DependencyClassifier):
+def cal_dep_score(
+    hunk: dict, file_content: str, dependency_analyzer: DependencyClassifier
+):
     def split2window_str(lines):
         windows = []
         for i in range(len(lines) // 10 + 1):
             if i == len(lines) // 10:
-                window = "".join(lines[i * 10:])
+                window = "".join(lines[i * 10 :])
             else:
-                window = "".join(lines[i * 10: (i + 1) * 10])
+                window = "".join(lines[i * 10 : (i + 1) * 10])
             windows.append(window)
         return windows
 
     fileB_lines = file_content.splitlines()
     # split file lines into code windows (10 lines)
-    hunk_window_str = "".join(hunk['code_window'])
+    hunk_window_str = "".join(hunk["code_window"])
     code_window_strsB = split2window_str(fileB_lines)
     if len(code_window_strsB) == 0:
-        raise KeyError('failed to split fileB into windows')
+        raise KeyError("failed to split fileB into windows")
     # form code windows pairs
-    code_window_pairs = [(hunk_window_str, windowB)
-                         for windowB in code_window_strsB]
+    code_window_pairs = [(hunk_window_str, windowB) for windowB in code_window_strsB]
     corpus_pair = dependency_analyzer.construct_corpus_pair(code_window_pairs)
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
--- line_locator/test_locator_metric.py
+++ line_locator/test_locator_metric.py
@@ -5,10 +5,8 @@ from locator_metric import calc_em_acc, calc_precision_recall_f1
 class TestMetrics(unittest.TestCase):
     def setUp(self):
         # Simulate predictions and ground truth
-        self.predictions = ['1\tkeep', '2\tadd',
-                            '3\treplace', '4\tkeep', '5\tadd']
-        self.ground_truth = ['1\tkeep', '2\tadd',
-                             '3\treplace', '4\tkeep', '5\tkeep']
+        self.predictions = ["1\tkeep", "2\tadd", "3\treplace", "4\tkeep", "5\tadd"]
+        self.ground_truth = ["1\tkeep", "2\tadd", "3\treplace", "4\tkeep", "5\tkeep"]
 
     def test_calc_em_acc(self):
         em, acc = calc_em_acc(self.predictions, self.ground_truth)
@@ -22,7 +20,8 @@ class TestMetrics(unittest.TestCase):
 
     def test_calc_precision_recall_f1(self):
         precision, recall, f1 = calc_precision_recall_f1(
-            self.predictions, self.ground_truth)
+            self.predictions, self.ground_truth
+        )
         # Expected Precision, Recall, F1 values
         expected_precision = 5 / 6
         expected_recall = 8 / 9
@@ -33,5 +32,5 @@ class TestMetrics(unittest.TestCase):
         self.assertAlmostEqual(f1, expected_f1, places=2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
